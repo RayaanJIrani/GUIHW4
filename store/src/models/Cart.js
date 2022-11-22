e7561961710071b68fb9ce8ba68b6262a1215864
RayaanJIrani <rayaanjirani@gmail.com>
2022-11-22 02:12:31 -0600

    Change to cart model.

--- store/src/models/Cart.js
+++ store/src/models/Cart.js
@@ -1,36 +1,15 @@
  export class Cart {
     constructor() {
-            this.items = [];
+        this.items = [];
+        this.total = 0;
+    }
+    addProduct(product) {
+        const cartItem = this.items.find(item => item.id === product.id);
+        if (cartItem) {
+            cartItem.quantity++;
+        } else {
+            this.items.push(new CartItem(product.id, product.name, product.price, 1));
         }
-
-        addItem(product, quantity) {
-            const existing = this.items.find(item => item.product.id === product.id);
-            if (existing) {
-                existing.quantity += quantity;
-            } else {
-                this.items.push({ product, quantity });
-            }
-        }
-
-        updateQuantity(product, quantity) {
-            const existing = this.items.find(item => item.product.id === product.id);
-            if (existing) {
-                existing.quantity = quantity;
-            }
-        }
-
-        removeItem(product) {
-            const existing = this.items.find(item => item.product.id === product.id);
-            if (existing) {
-                this.items = this.items.filter(item => item !== existing);
-            }
-        }
-
-        get itemCount() {
-            return this.items.reduce((total, item) => total + item.quantity, 0);
-        }
-
-        get totalPrice() {
-            return this.items.reduce((total, item) => total + item.quantity * item.product.price, 0);
-        }
+        this.total += product.price;
+    }
  }
