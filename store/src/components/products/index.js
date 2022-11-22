5a85a268130ebecea3748ae71a6c846e95302d06
RayaanJIrani <rayaanjirani@gmail.com>
2022-11-22 02:00:07 -0600

    Added empty files to match planed final file configuration.

--- store/src/components/products/index.js
+++ store/src/components/products/index.js
@@ -1,3 +1,5 @@
 export * from './productDetails';
 export * from './reviewList';
 export * from './reviewForm';
+export * from './MyCart';
+export * from './ProductList';
--- store/src/models/index.js
+++ store/src/models/index.js
@@ -1,2 +1,4 @@
 export * from './products';
 export * from './productReviews';
+export * from './Cart';
+export * from './CartItems';
