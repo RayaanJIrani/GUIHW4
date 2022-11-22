f00beb36deb785bcfde877d0470c07bc2ac6a485
RayaanJIrani <rayaanjirani@gmail.com>
2022-11-22 02:20:57 -0600

    Added API endpoints

--- store/src/api/productsAPI.js
+++ store/src/api/productsAPI.js
@@ -16,3 +16,23 @@ export const getProductById = (id) => new Promise((resolve, reject) => {
             reject(x);
         });
 });
+
+//GET products
+export const getProducts = () => new Promise( (resolve, reject) => {
+    axios.get(`${apiEndPoint}`, apiConfig)
+        .then(x => resolve(x.data))
+        .catch(x => {
+            alert(x);
+            reject(x);
+        });
+});
+
+//addReview
+export const addReview = (productId, review) => new Promise((resolve, reject) => {
+    axios.post(`${apiEndPoint}/${productId}/reviews`, review, apiConfig)
+        .then(x => resolve(x.data))
+        .catch(x => {
+            alert(x);
+            reject(x);
+        });
+});
