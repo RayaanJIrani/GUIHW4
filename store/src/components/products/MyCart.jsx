540ce35bee7d7120fc8b5ede7c188ab006d92e3c
RayaanJIrani <rayaanjirani@gmail.com>
2022-11-28 00:20:20 -0600

    Basic of the Cart done... now just have to do styling ugh.

--- store/src/components/products/MyCart.jsx
+++ store/src/components/products/MyCart.jsx
@@ -5,18 +5,27 @@ export const MyCart = () => {
     const {cart, addToCart} = useContext(CartContext);
 
     return <>
-    <button onClick={() => addToCart({id: 1, name: 'Product 1'})}>Add to cart</button>
+    <button onClick={() => addToCart({id: 1, name: "Hail", price: 18.54})}>Add to cart</button>
     <div className="container-lg">
-        <table className="table table-striped table-bordered mt-3">
+        <table className="table table-striped mt-3">
             <thead>
-                <th className="pt-2 pb-2">Qty</th>
-                <th className="pt-2 pb-2">Product</th>
-                <th className="pt-2 pb-2">Total</th>
+                <th className="pt-2 pb-2 col-1"> Qty</th>
+                <th className="pt-2 pb-2 col-10">Product</th>
+                <th className="pt-2 pb-2 col-1">Total</th>
             </thead>
             <tbody>
-                <tr><td>{cart.total}</td></tr>
+            {
+                cart.items.map(product => <tr>
+                    <td className="pt-2 pb-2 col-1 text-dark">{product.quantity}</td>
+                    <td className="pt-2 pb-2 col-10 text-dark">{product.product.name + " - " + product.product.price}</td>
+                    <td className="pt-2 pb-2 col-1 text-dark">{product.totalPrice}</td>
+                </tr>)
+            }
             </tbody>
         </table>
+    </div>
+    <div className="container-lg">
+        <p className="d-flex justify-content-end font-weight-bold">$ {cart.total}</p>
     </div>
     </>;
 };
