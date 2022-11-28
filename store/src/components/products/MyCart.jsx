5b09e79675777ff46ccd53806d18da7acefca479
RayaanJIrani <rayaanjirani@gmail.com>
2022-11-27 18:00:27 -0600

    Top outline

--- store/src/components/products/MyCart.jsx
+++ store/src/components/products/MyCart.jsx
@@ -1,5 +1,22 @@
+import {useContext} from "react";
+import {CartContext} from "../../context/CartContext";
+
 export const MyCart = () => {
+    const {cart, addToCart} = useContext(CartContext);
+
     return <>
-        <h2>My Cart</h2>
-    </>
-}
+    <button onClick={() => addToCart({id: 1, name: 'Product 1'})}>Add to cart</button>
+    <div className="container-lg">
+        <table className="table table-striped table-bordered mt-3">
+            <thead>
+                <th className="pt-2 pb-2">Qty</th>
+                <th className="pt-2 pb-2">Product</th>
+                <th className="pt-2 pb-2">Total</th>
+            </thead>
+            <tbody>
+                <tr><td>{cart.total}</td></tr>
+            </tbody>
+        </table>
+    </div>
+    </>;
+};
