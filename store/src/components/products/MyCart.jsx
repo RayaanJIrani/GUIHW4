621a8b86f7f0c3f71d09c9dbd96f27b721e01598
RayaanJIrani <rayaanjirani@gmail.com>
2022-11-28 00:57:12 -0600

    Did some CSS... need to do more for Cart

--- store/src/components/products/MyCart.jsx
+++ store/src/components/products/MyCart.jsx
@@ -5,19 +5,21 @@ export const MyCart = () => {
     const {cart, addToCart} = useContext(CartContext);
 
     return <>
-    <button onClick={() => addToCart({id: 1, name: "Hail", price: 18.54})}>Add to cart</button>
     <div className="container-lg">
         <table className="table table-striped mt-3">
-            <thead>
+            <thead className="border-top">
                 <th className="pt-2 pb-2 col-1"> Qty</th>
-                <th className="pt-2 pb-2 col-10">Product</th>
+                <th className="pt-2  pb-2 col-10">Product</th>
                 <th className="pt-2 pb-2 col-1">Total</th>
             </thead>
             <tbody>
             {
                 cart.items.map(product => <tr>
                     <td className="pt-2 pb-2 col-1 text-dark">{product.quantity}</td>
-                    <td className="pt-2 pb-2 col-10 text-dark">{product.product.name + " - " + product.product.price}</td>
+                    <td className="pt-2 pb-2 col-10 text-dark">
+                        <span>{product.product.name + " - "}</span>
+                        <span className="text-muted">${product.product.price}/each</span>
+                    </td>
                     <td className="pt-2 pb-2 col-1 text-dark">{product.totalPrice}</td>
                 </tr>)
             }
@@ -25,7 +27,8 @@ export const MyCart = () => {
         </table>
     </div>
     <div className="container-lg">
-        <p className="d-flex justify-content-end font-weight-bold">$ {cart.total}</p>
+        <p className="d-flex justify-content-end fw-bold">$ {cart.total}</p>
     </div>
+        <button onClick={() => addToCart({id: 1, name: "Hail", price: 18.54})}>Add to cart</button>
     </>;
 };
